c1436e34cf687c3dbb67cdc835bd5b44473a5936
Vishal-Desktop1 <Vishal-Desktop1@local.com>
2024-09-26 10:45:40 +0530

    With Opening
    With Opening

--- hello.sh
+++ hello.sh
@@ -1,5 +1,6 @@
-chmod +x hello.sh
+#!/bin/bash
 
+chmod +x hello.sh  # This line is not necessary in the script itself when you push it to the repo
 
 echo "Hello from Bash!"
-echo " Another Round"
+echo "Another Round"
