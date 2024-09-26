bf5d10250e71abe51bdfb2467d65ce65a4d828b6
Vishal-Desktop1 <Vishal-Desktop1@local.com>
2024-09-26 10:50:52 +0530

    Bck

--- hello.sh
+++ hello.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod +x hello.sh  # This line is not necessary in the script itself when you push it to the repo
+#chmod +x hello.sh  # This line is not necessary in the script itself when you push it to the repo
 
 echo "Hello from Bash!"
 echo "Another Round"
